a70246a7abae6034d2d067715a31c958c1dae9e0
Pat Barfield <patrickhbarfield@gmail.com>
2022-07-06 16:46:33 -0500

    init comit

---
+++ src/main/java/com/codeup/lightfoot/LightfootApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.lightfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightfootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightfootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/lightfoot/LightfootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.lightfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightfootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
