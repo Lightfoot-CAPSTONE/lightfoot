8fae95a8ed9acb2335e3e0c6ef121ec570924683
Pat Barfield <patrickhbarfield@gmail.com>
2022-07-19 13:51:34 -0500

    added Epa Aqs API

--- src/main/resources/static/js/views/AirNowAPI.js
+++
@@ -1,6 +0,0 @@
-import {AIRNOW} from "./keyfile.js";
-//API provides air quality data
-
-export default function getAirnow() {
-    airnow.accessToken = AIRNOW;
-}
---
+++ src/main/resources/static/js/views/EpaAqsApi.js
@@ -0,0 +1,4 @@
+import {EPA_AQS_API} from "./keyfile.js";
+
+export default function getEpaAqsApi(){
+    getEpaAqsApi.accessToken = EPA_AQS_API;
