f25e948aabc9013d5e2edc9ba9c747a25909cd87
Pat Barfield <patrickhbarfield@gmail.com>
2022-07-20 17:35:33 -0500

    added API for EPA air data

--- src/main/resources/static/js/views/EpaAqsApi.js
+++ src/main/resources/static/js/views/EpaAqsApi.js
@@ -1,5 +1,9 @@
-import {EPA_AQS_API} from "../keyfile.js";
+import {AIRNOW, EPA_AQS_API} from "../keyfile.js";
 
 export default function getEpaAqsApi() {
-    getEpaAqsApi.accessToken = EPA_AQS_API;
+    fetch(`https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=78602&distance=25&API_KEY=${EPA_AQS_API}`)
+        .then(rez=>rez.json())
+        .then(data=>console.log(data))
 }
+
+
